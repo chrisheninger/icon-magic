61bb8e13bf5197a13e05cb049761f03bec7906cc
Chris Heninger <chrish@synapsestudios.com>
2017-05-28 21:36:22 -0700

    ✨

--- icon-magic.js
+++ icon-magic.js
@@ -19,57 +19,67 @@ const icons = {
   512: 'pwa',
 };
 
-function optimizeImage(filename, cb) {
-  // https://github.com/imagemin/imagemin
-  imagemin(
-    [path.resolve(__dirname, `./.tmp/${filename}`)], // input file
-    'icons', // output dir
-    {
-      use: [imageminZopfli({ more: true })], // https://github.com/imagemin/imagemin-zopfli
-    }
-  ).then(() => {
-    console.log(`./${filename} ===> Optimization complete 🎉`);
-    cb();
+function optimizeImage(filename) {
+  return new Promise((resolve, reject) =>
+    // https://github.com/imagemin/imagemin
+    imagemin(
+      [path.resolve(__dirname, `./.tmp/${filename}`)], // input file
+      'icons', // output dir
+      {
+        use: [imageminZopfli({ more: true })], // https://github.com/imagemin/imagemin-zopfli
+      }
+    ).then(() => {
+      console.log(`✅  ./${filename} ===> Optimization complete 🎉`);
+      resolve();
+    })
+  ).catch(err => {
+    console.log(`❌  This is bad... I have no idea what's gone wrong.`, err);
   });
 }
 
-function resizeImage(imageBuffer, size, cb) {
+function resizeImage(imageBuffer, size) {
   const filename = icons[size] === 'favicon'
     ? 'favicon.png'
     : `${icons[size]}-icon-${size}x${size}.png`;
 
-  // https://github.com/aheckmann/gm
-  gm(imageBuffer)
-    // Validation
-    .format((err, value) => {
-      if (err) throw new Error(err);
-      if (value !== 'PNG')
-        throw new Error(
-          'Source image must be PNG format. See README.me for details.'
-        );
-    })
-    .size((err, value) => {
-      if (err) throw new Error(err);
-      if (value.width !== value.height)
-        throw new Error(
-          'Source image must be a square. See README.me for details.'
-        );
-      if (value.width < 512)
-        throw new Error(
-          'Source image dimensions must be at least 512px x 512px. See README.me for details.'
-        );
-    })
-    // Resizing
-    .resize(size, size)
-    .write(path.resolve(__dirname, `./.tmp/${filename}`), err => {
-      if (err) {
-        console.log(':( An error occured.', err);
-      } else {
-        console.log(`./${filename} ===> Resize complete 🎉`);
-      }
-      console.log(cb, filename);
-      cb(filename);
-    });
+  return new Promise((resolve, reject) =>
+    // https://github.com/aheckmann/gm
+    gm(imageBuffer)
+      // Validation
+      .format((err, value) => {
+        if (err) throw new Error(err);
+        if (value !== 'PNG')
+          throw new Error(
+            '❌  Source image must be PNG format. See README.me for details.'
+          );
+      })
+      .size((err, value) => {
+        if (err) throw new Error(err);
+        if (value.width !== value.height)
+          throw new Error(
+            '❌  Source image must be a square. See README.me for details.'
+          );
+        if (value.width < 512)
+          throw new Error(
+            '❌  Source image dimensions must be at least 512px x 512px. See README.me for details.'
+          );
+      })
+      // Resizing
+      .resize(size, size)
+      .write(path.resolve(__dirname, `./.tmp/${filename}`), err => {
+        if (err) {
+          console.error(
+            `❌  This is bad... I have no idea what's gone wrong.`,
+            err
+          );
+        } else {
+          console.log(`✅  ./${filename} ===> Resize complete 🎉`);
+        }
+        resolve(filename);
+      })
+  ).catch(err => {
+    console.log(`❌  This is bad... I have no idea what's gone wrong.`, err);
+  });
 }
 
 // args[0] == node bin path
@@ -81,33 +91,30 @@ if (fs.existsSync(args[2])) {
     if (err) console.log(':( An error occured.', err);
     if (!fs.existsSync('.tmp')) fs.mkdirSync('.tmp');
     if (fs.existsSync('icons')) rimraf('icons', () => {});
-
-    console.log('⏱  Resizing...');
-    let resizePass = Object.keys(icons).map(
-      size =>
-        new Promise(resolve => {
-          resizeImage(imageBuffer, size, resolve);
-        })
+    // Iterate over our options object `icons` and begin resizing images
+    console.log('📏  Resizing...');
+    let resizePass = Object.keys(icons).map(size =>
+      resizeImage(imageBuffer, size)
     );
-
+    // Once resized image promise resolves...
     Promise.all(resizePass)
       .then(resizedImages => {
         console.log('✅  Success!\n\n');
-
+        // ...pass them into the optimization function
         console.log(
-          '⏱  Optimizing... larger icons will take longer... use the fifth P!'
+          '⏱  Optimizing... larger icons will take longer to optimize... use the fifth P!'
         );
-        let optimizationPass = resizedImages.map(
-          filename =>
-            new Promise(resolve => {
-              optimizeImage(filename, resolve);
-            })
+        console.log(
+          "🚗  Note– order doesn't matter– we've gotta wait for the slowest regardless."
         );
-
+        let optimizationPass = resizedImages.map(filename =>
+          optimizeImage(filename)
+        );
+        // Once optimization promise resolves...
         Promise.all(optimizationPass).then(() => {
           console.log('✅  Success! Thanks for being patient.\n\n');
-
-          console.log('⏱  Cleaning Up...');
+          // ...remove the .tmp directory we used earlier
+          console.log('🗑  Cleaning Up...');
           rimraf('.tmp', () => {
             console.log('✅  Success!');
           });
